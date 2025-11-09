2c0abf8d84fb5dbe0966914f6acb755f613c5e89
junu <cjw8809@gmail.com>
2025-11-09 18:44:18 +0900

    packages 의 eslint에 rootDir 명시

--- packages/node-jit/eslint.config.js
+++ packages/node-jit/eslint.config.js
@@ -1,7 +1,15 @@
-import { nodeJsConfig } from "@repo/eslint-config/node";
+import { nodeJsConfig } from '@repo/eslint-config/node';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default [
   {
+    languageOptions: {
+      parserOptions: {
+        rootDir: __dirname,
+      },
+    },
     ignores: ['**/*.json', 'eslint.config.js'],
   },
   ...nodeJsConfig,
--- packages/react-jit/eslint.config.js
+++ packages/react-jit/eslint.config.js
@@ -1,7 +1,15 @@
-import { reactInternalConfig } from "@repo/eslint-config/react-internal";
+import { reactInternalConfig } from '@repo/eslint-config/react-internal';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default [
   {
+    languageOptions: {
+      parserOptions: {
+        rootDir: __dirname,
+      },
+    },
     ignores: ['**/*.json', 'eslint.config.js'],
   },
   ...reactInternalConfig,
